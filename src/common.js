8705c84e0046d8305e6d4f01fadced7e7b4e7ed7
周洋光 <61733195+YangguangZhou@users.noreply.github.com>
2021-10-08 21:57:21 +0800

    Update common.js

--- src/common.js
+++ src/common.js
@@ -208,13 +208,13 @@ const renderAboutText = (userType,followerCount,followingCount,ranking,slogan,da
     let line1 = `<g transform="translate(25,0)"><text x="0" y="15" class="about-text">用户类型：${userType}</text></g>`
     let line2 = `<g transform="translate(25,30)"><text x="0" y="15" class="about-text">TA关注了${followingCount}人，共有${followerCount}人关注TA</text></g>`
     let line3 = "";
-    if(ranking==null)
+    if(ranking)
     {
-        line3 = `<g transform="translate(25,60)"><text x="0" y="15" class="about-text-grey">暂无咕值排名</text></g>`
+        line3 = `<g transform="translate(25,60)"><text x="0" y="15" class="about-text-grey">咕值排名：第${ranking}名</text></g>`
     }
     else
     {
-        line3 = `<g transform="translate(25,60)"><text x="0" y="15" class="about-text">咕值排名：第${ranking}名</text></g>`
+        line3 = `<g transform="translate(25,60)"><text x="0" y="15" class="about-text">暂无咕值排名</text></g>`
     }
     let line4 = "";
     if(slogan)
