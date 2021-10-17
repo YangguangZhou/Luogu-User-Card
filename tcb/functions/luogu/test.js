b47b479d1981e78854b59ebc315cc4a5156e229c
周洋光 <yanguangzhousunny@163.com>
2021-10-17 17:22:54 +0800

    tcb

---
+++ tcb/functions/luogu/test.js
@@ -0,0 +1,14 @@
+// const { main } = require('./index');
+
+// main({
+//   queryStringParameters: {
+//     id: 313209,
+//   },
+//   path: '/practice',
+// }).then(res => console.log(res));
+
+///////
+
+// const fetchStats = require('./fetchStats');
+
+// fetchStats(313209).then(res => console.log(res));
