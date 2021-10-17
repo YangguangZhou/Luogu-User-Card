68964a6b424d32e841dc5d7fef941909fa44b6e2
周洋光 <yanguangzhousunny@163.com>
2021-10-17 19:02:20 +0800

    Update guzhi-card.js

--- src/guzhi-card.js
+++ src/guzhi-card.js
@@ -2,8 +2,49 @@ const {
     Card,
     renderError,
     renderChart,
+    renderAboutText,
     renderNameTitle,
 } = require("./common.js");
+
+/**
+ * 
+ * @param {number} id 用户id
+ * @returns {Object} 获取的用户数据 {name, color, ccfLevel, slogan, followerCount, followingCount}
+ */
+ async function fetchAbout(id) {
+    //debug 测试请求
+    //const res = await axios.get(`https://tc-0glpuj1k4e75e5ec-1300876583.ap-shanghai.service.tcloudbase.com/luogu?id=${id}`);
+
+    const res = await axios.get(`https://www.luogu.com.cn/user/${id}?_contentOnly`)
+
+    const about = {
+        name: "NULL",
+        color: "Gray",
+        ccfLevel: 0,
+        slogan: "",
+        followerCount: 0,
+        followingCount:0,
+        ranking:-1,
+        userType:"Not found.",
+        tag:""
+    }
+    if(res.data.code !== 200) {
+        return about;
+    }
+    const user = res.data.currentData.user;
+    
+    about.name = decodeURI(user.name);
+    about.color = decodeURI(user.color);
+    about.ccfLevel = user.ccfLevel;
+    about.slogan = decodeURI(user.slogan);
+    about.followerCount = user.followerCount;
+    about.followingCount = user.followingCount;
+    about.ranking = user.ranking;
+    about.userType = user.isAdmin?"管理员":(user.isBanned?"封禁用户":"普通用户");
+    about.tag = decodeURI(user.badge);
+    
+    return about;
+}
 
 const renderGuzhiCard = (userInfo, scores, options) => {
     const regNum = /^\d*$/;
@@ -66,4 +107,4 @@ const renderGuzhiCard = (userInfo, scores, options) => {
     }).render();
 }
 
-module.exports = { renderGuzhiCard }
+module.exports = { fetchAbout,renderGuzhiCard }
