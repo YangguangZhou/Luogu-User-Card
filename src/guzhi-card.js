d92d2004d1ef9e567c063c622ea5c7bb3cd1f8d4
周洋光 <yanguangzhousunny@163.com>
2021-10-17 18:55:22 +0800

    Update guzhi-card.js

--- src/guzhi-card.js
+++ src/guzhi-card.js
@@ -24,6 +24,7 @@ const renderGuzhiCard = (userInfo, scores, options) => {
         name,
         color,
         ccfLevel,
+        ranking,
         tag
     } = userInfo || {};
 
@@ -51,7 +52,7 @@ const renderGuzhiCard = (userInfo, scores, options) => {
         {label: "获得成就", data: scoreArray[4], color: getScoreColor(scoreArray[4])},
     ]
 
-    const title = userInfo != null ? renderNameTitle(name, color, ccfLevel, "的咕值信息", cardWidth, `总咕值: ${scoreSum}分`, tag) : "";
+    const title = userInfo != null ? renderNameTitle(name, color, ccfLevel, "的咕值信息", cardWidth, `总咕值: ${scoreSum}分\n咕值排名：${ranking}`, tag) : "";
 
     const body = renderChart(datas, labelWidth, progressWidth, "分");
 
